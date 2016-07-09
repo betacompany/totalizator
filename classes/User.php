1a7cd3258e1ba8cd48d84b2637b945341c4634a1
Bob <boris.v.smirnov@gmail.com>
2016-07-09 17:48:51 +0300

    sorting rating by valued guesses

--- classes/User.php
+++ classes/User.php
@@ -74,46 +74,6 @@ class User
         return $sum;
     }
 
-    public function getGuessedOutcomes($comp_id = 0)
-    {
-        if ($comp_id != 0) {
-            $req = mysql_qw('SELECT COUNT(*) AS `scores` FROM `total_stakes` INNER JOIN `total_matches` ON `total_stakes`.`match_id`=`total_matches`.`id` WHERE `uid`=? AND `comp_id`=? AND `total_stakes`.`score`>0', $this->getId(), $comp_id);
-            return mysql_result($req, 0, 'scores');
-        }
-
-        $sum = 0;
-        foreach (Competition::getAll() as $competition) {
-            $sum += intval($this->getGuessedOutcomes($competition->getId()));
-        }
-
-        return $sum;
-    }
-
-    public static function getGuessesOrdered($comp_id = 0)
-    {
-        if ($comp_id != 0) {
-            $req = mysql_qw('SELECT `total_stakes`.`uid` AS uid,
-              SUM(`total_stakes`.`score`) AS points,
-              SUM(IF(`total_stakes`.`score` = 4, 1, 0)) AS count4,
-              SUM(IF(`total_stakes`.`score` = 3, 1, 0)) AS count3,
-              SUM(IF(`total_stakes`.`score` = 2, 1, 0)) AS count2,
-              SUM(IF(`total_stakes`.`score` = 1, 1, 0)) AS count1
-              FROM `total_stakes`
-              INNER JOIN `total_matches` ON `total_stakes`.`match_id`=`total_matches`.`id`
-              WHERE `comp_id`=?
-              GROUP BY uid
-              ORDER BY points DESC, count4 DESC, count3 DESC, count2 DESC, count1 DESC', $comp_id);
-            return mysql_result($req, 0);
-        }
-
-        $sum = 0;
-        foreach (Competition::getAll() as $competition) {
-            $sum += intval(User::getGuessesOrdered($competition->getId()));
-        }
-
-        return $sum;
-    }
-
     public function getPlayedStakes($compId = 0)
     {
         return Stake::getPlayedFor($this->getId(), $compId);
@@ -122,7 +82,7 @@ class User
     public static function getAllByRating($comp_id = 0)
     {
         if ($comp_id == 0) {
-            $q = mysql_qw('SELECT `total_stakes`.`uid`,
+            $qwery = mysql_qw('SELECT `total_stakes`.`uid`,
               SUM(`total_stakes`.`score`) AS scores,
               SUM(IF(`total_stakes`.`score` = 4, 1, 0)) AS count4,
               SUM(IF(`total_stakes`.`score` = 3, 1, 0)) AS count3,
@@ -132,7 +92,7 @@ class User
               GROUP BY uid
               ORDER BY scores DESC, count4 DESC, count3 DESC, count2 DESC, count1 DESC');
         } else {
-            $q = mysql_qw('SELECT `total_stakes`.`uid`,
+            $qwery = mysql_qw('SELECT `total_stakes`.`uid`,
               SUM(`total_stakes`.`score`) AS scores,
               SUM(IF(`total_stakes`.`score` = 4, 1, 0)) AS count4,
               SUM(IF(`total_stakes`.`score` = 3, 1, 0)) AS count3,
@@ -146,27 +106,44 @@ class User
 
         $data = array();
         $i = 0;
-        while ($row = mysql_fetch_assoc($q)) {
+        $previous_user_score = array(
+            "score" => 0,
+            "count4" => 0,
+            "count3" => 0,
+            "count2" => 0,
+            "count1" => 0
+        );
+        while ($row = mysql_fetch_assoc($qwery)) {
             try {
                 $user = new User($row['uid']);
                 $data[$i]['user'] = $user;
                 $data[$i]['scores'] = $row['scores'];
+                $data[$i]['sort_info'] = "";
+                $this_scores = array(
+                    "score" => $row['count4'],
+                    "4" => $row['count4'],
+                    "3" => $row['count3'],
+                    "2" => $row['count2'],
+                    "1" => $row['count1']
+                );
+                if ($previous_user_score["score"] == $row['scores']) {
+                    foreach ($this_scores as $category => $value) {
+                        $prev_value = $previous_user_score[$category];
+                        if ($value < $prev_value) {
+                            $pts = $category == 1 ? "очку" : "очка";
+                            $data[$i - 1]['sort_info'] = "больше угаданных ставок по $category $pts: $prev_value против $value";
+                            break;
+                        } else {
+                            $data[$i - 1]['sort_info'] = "соперники абсолютно равны";
+                        }
+                    }
+                }
+                $previous_user_score = $this_scores;
                 $i++;
             } catch (Exception $e) {
             }
         }
         return $data;
-    }
-
-    function compare_by_score_then_by_valued_guesses($a, $b)
-    {
-        if ($a['scores'] > $b['scores']) return 1;
-        elseif ($a['scores'] < $b['scores']) return -1;
-        for ($i = 4; $i >= 1; $i--) {
-            if ($a['point_stats'][$i] > $b['point_stats'][$i]) return 1;
-            elseif ($a['point_stats'][$i] < $b['point_stats'][$i]) return -1;
-        }
-        return 0;
     }
 
 }
