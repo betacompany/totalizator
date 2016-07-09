4c3b6b557a606639cfabb72f7cb8ca82093d0d97
Bob <boris.v.smirnov@gmail.com>
2016-07-09 13:36:17 +0300

    sorting rating by valued guesses

--- classes/User.php
+++ classes/User.php
@@ -160,17 +160,9 @@ class User
               GROUP BY uid
               ORDER BY points DESC, count4 DESC, count3 DESC, count2 DESC, count1 DESC';
 
-        $q = mysql_query('SELECT `total_stakes`.`uid` AS uid
-//          `total_matches`.`comp_id` AS comp,
-//          SUM(`total_stakes`.`score`) AS points,
-//          SUM(IF(`total_stakes`.`score` = 4, 1, 0)) AS count4,
-//          SUM(IF(`total_stakes`.`score` = 3, 1, 0)) AS count3,
-//          SUM(IF(`total_stakes`.`score` = 2, 1, 0)) AS count2,
-//          SUM(IF(`total_stakes`.`score` = 1, 1, 0)) AS count1
-          FROM (`total_stakes` INNER JOIN `total_matches` ON `total_stakes`.`match_id`=`total_matches`.`id`)
-          WHERE `total_matches`.`comp_id`=?
-          GROUP BY uid
-          ORDER BY points DESC, count4 DESC, count3 DESC, count2 DESC, count1 DESC', 10);
+        $q = mysql_query('SELECT DISTINCT(`total_stakes`.`uid`) FROM
+                (`total_stakes` INNER JOIN `total_matches` ON `total_stakes`.`match_id`=`total_matches`.`id`)
+                WHERE `total_matches`.`comp_id`=?', 10);
         while ($row = mysql_fetch_assoc($q)) {
             foreach ($row as $name => $value) {
                 print "$name: $value\t";
