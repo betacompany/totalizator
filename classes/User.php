6e27b5dbd5004fe1dd9f69597615a8c33549bbd0
Bob <boris.v.smirnov@gmail.com>
2016-07-09 12:37:49 +0300

    sorting rating by valued guesses

--- classes/User.php
+++ classes/User.php
@@ -161,10 +161,15 @@ class User
               ORDER BY points DESC, count4 DESC, count3 DESC, count2 DESC, count1 DESC';
 
         $q = mysql_query('SELECT `total_stakes`.`uid` AS uid,
-          SUM(`total_stakes`.`score`) AS points
+          SUM(`total_stakes`.`score`) AS points,
+          SUM(IF(`total_stakes`.`score` = 4, 1, 0)) AS count4,
+          SUM(IF(`total_stakes`.`score` = 3, 1, 0)) AS count3,
+          SUM(IF(`total_stakes`.`score` = 2, 1, 0)) AS count2,
+          SUM(IF(`total_stakes`.`score` = 1, 1, 0)) AS count1
           FROM `total_stakes`
           INNER JOIN `total_matches` ON `total_stakes`.`match_id`=`total_matches`.`id`
-          GROUP BY uid');
+          GROUP BY uid
+          ORDER BY points DESC, count4 DESC, count3 DESC, count2 DESC, count1 DESC');
         while ($row = mysql_fetch_assoc($q)) {
             foreach ($row as $name => $value) {
                 print "$name: $value\t";
