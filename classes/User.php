df17b28f80cb4f969d62b349f11a9ac6ec759863
Boris Smirnov <boris.v.smirnov@gmail.com>
2016-07-22 20:45:08 +0300

    Enhanced rating sort (#9)
    * looking for a debugging output
    
    * looking for a debugging output
    
    * looking for a debugging output
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * sorting rating by valued guesses
    
    * adding nice explanation
    
    * adding nice explanation
    
    * adding nice explanation
    
    * adding nice explanation
    
    * ui improvements
    
    * ui improvements
    
    * ui improvements
    
    * ui improvements
    
    * ui improvements
    
    * ui improvements
    
    * ui improvements
    
    * ui improvements
    
    * ui improvements
    
    * ui improvements
    
    * ui improvements
    
    * noted in rules
    
    * rules misspell and codestyle fixed
    
    * fancy pop-on-click implemented
    
    * fancy pop-on-click implemented
    
    * fancy pop-on-click implemented
    
    * fancy pop-on-click implemented
    
    * fancy pop-on-click implemented
    
    * fancy pop-on-click implemented
    
    * fancy pop-on-click clean-up
    
    * fancy pop-on-click clean-up
    
    * fancy pop-on-click clean-up
    
    * fancy pop-on-click clean-up
    
    * fancy pop-on-click clean-up
    
    * fancy pop-on-click clean-up
    
    * fancy pop-on-click clean-up
    
    * fancy pop-on-click clean-up

--- classes/User.php
+++ classes/User.php
@@ -54,7 +54,7 @@ class User {
 
     public function getScores($comp_id = 0) {
         if ($comp_id != 0) {
-            $req = mysql_qw('SELECT SUM(`score`) AS `scores` FROM `total_stakes` INNER JOIN `total_matches` ON `total_stakes`.`match_id`=`total_matches`.`id` WHERE `uid`=? and `comp_id`=?', $this->getId(), $comp_id);
+            $req = mysql_qw('SELECT SUM(`score`) AS `scores` FROM `total_stakes` INNER JOIN `total_matches` ON `total_stakes`.`match_id`=`total_matches`.`id` WHERE `uid`=? AND `comp_id`=?', $this->getId(), $comp_id);
             return mysql_result($req, 0, 'scores');
         }
 
@@ -66,53 +66,73 @@ class User {
         return $sum;
     }
 
-    public function getGuessed($comp_id = 0) {
-        if ($comp_id != 0) {
-            $req = mysql_qw('SELECT COUNT(*) AS `scores` FROM `total_stakes` INNER JOIN `total_matches` ON `total_stakes`.`match_id`=`total_matches`.`id` WHERE `uid`=? and `comp_id`=? and `total_stakes`.`score`>0', $this->getId(), $comp_id);
-            return mysql_result($req, 0, 'scores');
-        }
-
-        $sum = 0;
-        foreach (Competition::getAll() as $competition) {
-            $sum += intval($this->getGuessed($competition->getId()));
-        }
-
-        return $sum;
-    }
-
     public function getPlayedStakes($compId = 0) {
         return Stake::getPlayedFor($this->getId(), $compId);
     }
 
     public static function getAllByRating($comp_id = 0) {
+        if ($comp_id == 0) {
+            $qwery = mysql_qw('SELECT `total_stakes`.`uid`,
+              SUM(`total_stakes`.`score`) AS scores,
+              SUM(IF(`total_stakes`.`score` = 4, 1, 0)) AS count4,
+              SUM(IF(`total_stakes`.`score` = 3, 1, 0)) AS count3,
+              SUM(IF(`total_stakes`.`score` = 2, 1, 0)) AS count2,
+              SUM(IF(`total_stakes`.`score` = 1, 1, 0)) AS count1
+              FROM (`total_stakes` INNER JOIN `total_matches` ON `total_stakes`.`match_id`=`total_matches`.`id`)
+              GROUP BY uid
+              ORDER BY scores DESC, count4 DESC, count3 DESC, count2 DESC, count1 DESC');
+        } else {
+            $qwery = mysql_qw('SELECT `total_stakes`.`uid`,
+              SUM(`total_stakes`.`score`) AS scores,
+              SUM(IF(`total_stakes`.`score` = 4, 1, 0)) AS count4,
+              SUM(IF(`total_stakes`.`score` = 3, 1, 0)) AS count3,
+              SUM(IF(`total_stakes`.`score` = 2, 1, 0)) AS count2,
+              SUM(IF(`total_stakes`.`score` = 1, 1, 0)) AS count1
+              FROM (`total_stakes` INNER JOIN `total_matches` ON `total_stakes`.`match_id`=`total_matches`.`id`)
+              WHERE `total_matches`.`comp_id`=?
+              GROUP BY uid
+              ORDER BY scores DESC, count4 DESC, count3 DESC, count2 DESC, count1 DESC', $comp_id);
+        }
+
         $data = array();
         $i = 0;
-        if ($comp_id == 0) {
-            $req = mysql_qw('SELECT DISTINCT(`total_stakes`.`uid`) FROM
-                (`total_stakes` INNER JOIN `total_matches` ON `total_stakes`.`match_id`=`total_matches`.`id`)
-                WHERE 1=1');
-        } else {
-            $req = mysql_qw('SELECT DISTINCT(`total_stakes`.`uid`) FROM
-                (`total_stakes` INNER JOIN `total_matches` ON `total_stakes`.`match_id`=`total_matches`.`id`)
-                WHERE `total_matches`.`comp_id`=?', $comp_id);
-        }
-        while ($u = mysql_fetch_assoc($req)) {
+        $previous_user_score = array(
+            "score" => 0,
+            "count4" => 0,
+            "count3" => 0,
+            "count2" => 0,
+            "count1" => 0
+        );
+        while ($row = mysql_fetch_assoc($qwery)) {
             try {
-                $user = new User($u['uid']);
-                $scores = $user->getScores($comp_id);
-                $guessed = $user->getGuessed($comp_id);
-
-                $data[$i]['scores'] = $scores;
-                $data[$i]['guessed'] = $guessed;
+                $user = new User($row['uid']);
                 $data[$i]['user'] = $user;
+                $data[$i]['scores'] = $row['scores'];
+                $data[$i]['sort_info'] = "";
+                $this_scores = array(
+                    "score" => $row['scores'],
+                    "4" => $row['count4'],
+                    "3" => $row['count3'],
+                    "2" => $row['count2'],
+                    "1" => $row['count1']
+                );
+                if ($previous_user_score["score"] == $row['scores']) {
+                    foreach ($this_scores as $category => $value) {
+                        $prev_value = $previous_user_score[$category];
+                        if ($value < $prev_value) {
+                            $pts = $category == 1 ? "очку" : "очка";
+                            $data[$i - 1]['sort_info'] = "больше угаданных ставок по $category $pts: $prev_value против $value";
+                            break;
+                        } else {
+                            $data[$i - 1]['sort_info'] = "EQUAL";
+                        }
+                    }
+                }
+                $previous_user_score = $this_scores;
                 $i++;
             } catch (Exception $e) {
-
             }
         }
-
-        rsort($data);
-
         return $data;
     }
 
