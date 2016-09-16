34ce9cd1fff1f69ed404ddd504b6454bbffaf0f4
bobsmirnov <bobsmirnov@yandex-team.ru>
2016-09-16 22:41:28 +0300

    all stakes debug output

--- processor.php
+++ processor.php
@@ -67,6 +67,8 @@ switch ($_REQUEST['action']) {
             if (!$match->isAvailableFor(userid())) {
                 $stakes = Stake::getByMatchId($_REQUEST['match_id']);
                 echo '<div class="row-fluid">';
+                print "stakes";
+                print_r($stakes);
                 out("{$match->getCompetitor1()->getName()} победит", filter_and_sort($stakes, Stake::WIN1));
                 out("ничья", filter_and_sort($stakes, Stake::DRAW));
                 out("{$match->getCompetitor2()->getName()} победит", filter_and_sort($stakes, Stake::WIN2));
