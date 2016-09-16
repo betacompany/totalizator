910b2a5a5616a8f8ebbe2bb82d3d5cbec7a07064
bobsmirnov <bobsmirnov@yandex-team.ru>
2016-09-16 22:40:05 +0300

    Correct default rating load

--- rating.php
+++ rating.php
@@ -38,7 +38,7 @@ require_once dirname(__FILE__) . '/classes/User.php';
                 </li>
             </ul>
 
-            <script type="text/javascript">loadRating(0);</script>
+            <script type="text/javascript">loadRating(<?=$competitions[0]?>);</script>
         </div>
 
         <div class="span8">
