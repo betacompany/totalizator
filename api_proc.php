8acd0e6d7c38eede04a03b9821d6271a8c6e07d4
Artyom Grigoriev <ortemij@yandex-team.ru>
2020-10-28 21:36:50 +0300

    fix

--- api_proc.php
+++ api_proc.php
@@ -12,6 +12,6 @@ require_once dirname(__FILE__) . '/classes/Stake.php';
 
 switch ($_REQUEST['action']) {
     case 'import_ext_match':
-        $data = json_decode(file_get_contents("php://input"));
+        $data = json_decode(file_get_contents("php://input"), true);
         echo $data['ext_id'];
 }
