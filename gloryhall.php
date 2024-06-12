92028a244865dac5344d1d31c49439068923fb04
Artem Grigorev <ortemij@gmail.com>
2024-06-12 23:18:48 +0200

    Showing by brackets

--- gloryhall.php
+++ gloryhall.php
@@ -11,6 +11,27 @@ require_once dirname(__FILE__) . '/classes/Leaderboard.php';
 
 $data = Leaderboard::getGloryHall();
 
+$upper_bracket = array();
+$middle_bracket = array();
+$lower_bracket = array();
+
+$maxCount = max(array_column($data, 'count'));
+$minCount = min(array_column($data, 'count'));
+
+foreach ($data as $item) {
+    if ($item['count'] == $maxCount) {
+        $upper_bracket[] = $item;
+    } elseif ($item['count'] == $minCount) {
+        $lower_bracket[] = $item;
+    } else {
+        $middle_bracket[] = $item;
+    }
+}
+
+function compile_row($item) {
+    return $item['user']->getSNnbsp() . " " . str_repeat("&#127942;", $item['count']);
+}
+
 ?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 
@@ -21,12 +42,16 @@ $data = Leaderboard::getGloryHall();
 <div class="container">
     <? include 'templates/menu.php'; ?>
 
-    <div class="row">
+    <div class="row" style="margin-top: 100px; margin-bottom: 100px">
         <div class="span8 offset2">
-            <? foreach ($data as $item) { ?>
-
-                <h3><?= $item['user']->getSNnbsp() . " " . str_repeat("&#127942;", $item['count']) ?></h3>
-            
+            <? foreach ($upper_bracket as $item) { ?>
+            <h1><?= compile_row($item) ?></h1>
+            <? } ?>
+            <? foreach ($middle_bracket as $item) { ?>
+            <h2><?= compile_row($item) ?></h2>
+            <? } ?>
+            <? foreach ($lower_bracket as $item) { ?>
+            <h3><?= compile_row($item) ?></h3>
             <? } ?>
         </div>
     </div>
